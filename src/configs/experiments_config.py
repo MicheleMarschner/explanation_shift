2976bc7bd13b461f832a1e5bbcecc546af260fb3
Michele Marschner <michele.marschner@gmail.com>
2026-03-30 22:28:56 +0200

    finished experiments

--- src/configs/experiments_config.py
+++ src/configs/experiments_config.py
@@ -31,7 +31,7 @@ EXP_CONFIGS = ExperimentTemplate(
     N_PAIRS = 1000,
     CORRUPTIONS = ["gaussian_noise", "defocus_blur", "brightness", "fog"], 
     SEVERITIES  = [1, 2, 3, 5],
-    EXPLAINERS = ["IG", "GradCAM"],
+    EXPLAINERS = ["GradCAM"],
     SEEDS = [7, 42, 52, 128, 1200]
 )
 
